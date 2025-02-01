c5b289e2651160887f4e365b5c5c3d2944ad532e
virtualmartire <boomscippo@gmail.com>
2025-02-01 11:18:46 +0100

    order

--- main.py
+++ main.py
@@ -4,25 +4,44 @@ import json
 with open('keys/real-keys.json', 'r') as f:
     keys_dict = json.load(f)
 
-# ENDPOINT = "https://api.fireworks.ai/inference/v1/chat/completions"
-# API_KEY = keys_dict['fireworks']
-# MODEL_NAME = "accounts/fireworks/models/llama-v3p1-8b-instruct"
-# ENDPOINT = "https://api.deepinfra.com/v1/openai"
-# API_KEY = keys_dict['deepinfra']
-# MODEL_NAME = "meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo"
-ENDPOINT = "https://api.studio.nebius.ai/v1/"
-API_KEY = keys_dict['nebius']
-MODEL_NAME = "meta-llama/Meta-Llama-3.1-8B-Instruct-fast"
+#
+##
+### PROVIDERS CONFIG
+##
+#
+
+fireworks_config = {
+    "endpoint": "https://api.fireworks.ai/inference/v1/chat/completions",
+    "api_key": keys_dict['fireworks'],
+    "model_name": "accounts/fireworks/models/llama-v3p3-70b-instruct"
+}
+
+deepinfra_config = {
+    "endpoint": "https://api.deepinfra.com/v1/openai",
+    "api_key": keys_dict['deepinfra'],
+    "model_name": "meta-llama/Llama-3.3-70B-Instruct"
+}
+
+nebius_config = {
+    "endpoint": "https://api.studio.nebius.ai/v1/",
+    "api_key": keys_dict['nebius'],
+    "model_name": "meta-llama/Llama-3.3-70B-Instruct"
+}
+
+#
+##
+### RUN
+##
+#
 
 PROMPT = "Write a half-page paragraph about technological innovation."
 NUM_GENERATIONS = 10
+chosen_provider = nebius_config
 
 results = utils.measureTokenGenerationSpeed(
-    API_KEY, 
-    ENDPOINT, 
-    MODEL_NAME, 
-    PROMPT,
-    NUM_GENERATIONS
+    **chosen_provider,
+    prompt=PROMPT,
+    num_generations=NUM_GENERATIONS,
 )
 
 print()
